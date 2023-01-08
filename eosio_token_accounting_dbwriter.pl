7bf6f9bbb1ebc6b7922ef2870a9734d5542f2b69
cc32d9 <cc32d9@gmail.com>
2023-01-08 22:12:07 +0000

    new field in SYNC: block_time

--- eosio_token_accounting_dbwriter.pl
+++ eosio_token_accounting_dbwriter.pl
@@ -91,8 +91,8 @@ my $sth_add_failure = $dbh->prepare
 
 
 my $sth_upd_sync = $dbh->prepare
-    ('INSERT INTO SYNC (network, block_num) VALUES(?,?) ' .
-     'ON DUPLICATE KEY UPDATE block_num=?');
+    ('INSERT INTO SYNC (network, block_num, block_time) VALUES(?,?,?) ' .
+     'ON DUPLICATE KEY UPDATE block_num=?, block_time=?');
 
 my $committed_block = 0;
 my $stored_block = 0;
@@ -230,7 +230,9 @@ sub process_data
 
             if( $uncommitted_block > $stored_block )
             {
-                $sth_upd_sync->execute($network, $uncommitted_block, $uncommitted_block);
+                my $block_time = $data->{'block_timestamp'};
+                $block_time =~ s/T/ /;
+                $sth_upd_sync->execute($network, $uncommitted_block, $block_time, $uncommitted_block, $block_time);
                 $dbh->commit();
                 $stored_block = $uncommitted_block;
             }
---
+++ patch_01_block_time_in_SYNC.sh
@@ -0,0 +1,2 @@
+mysql --database=eosio_token_accounting --execute='ALTER TABLE SYNC ADD COLUMN block_time DATETIME NOT NULL'
+                             
